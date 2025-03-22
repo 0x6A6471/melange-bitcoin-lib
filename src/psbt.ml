96a754e1b8f344fafb78def63b87bd88286a5821
0x6A6471 <hello@jakequinter.io>
2025-03-22 08:52:17 -0500

    getTxOutputs

--- src/psbt.ml
+++ src/psbt.ml
@@ -1,3 +1,20 @@
+type hash_input = String of string | Bytes of Js.uint8Array
+
+type transaction_input = {
+  hash : hash_input;
+  index : int;
+  sequence : int option;
+}
+
+type psbt_tx_input = { hash : bytes; index : int; sequence : int option }
+type transaction_output = { script : Js.uint8Array; value : Js.bigint }
+
+type psbt_tx_output = {
+  script : Js.uint8Array;
+  value : Js.bigint;
+  address : string option;
+}
+
 type t
 type psbt_opts = { network : Network.t option; maximum_fee_rate : float option }
 
@@ -5,3 +22,5 @@ let default_opts = { network = None; maximum_fee_rate = None }
 
 external fromHex : string -> ?psbt_opts:psbt_opts -> unit -> t = "fromHex"
 [@@mel.module "bitcoinjs-lib"] [@@mel.scope "Psbt"]
+
+external getTxOutputs : t -> psbt_tx_output array = "txOutputs" [@@mel.get]
