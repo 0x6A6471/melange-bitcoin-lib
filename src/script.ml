6a8b03355ff65132f65561ae6ad7e70c430b9382
0x6A6471 <hello@jakequinter.io>
2025-03-28 10:39:18 -0500

    list -> array

--- src/script.ml
+++ src/script.ml
@@ -2,7 +2,7 @@ type chunk = Buffer of Js.Typed_array.Uint8Array.t | Opcode of int
 
 type chunks =
   | BufferChunks of Js.Typed_array.Uint8Array.t
-  | StackChunks of chunk list
+  | StackChunks of chunk array
 
 external toASM : chunks -> string = "toASM"
 [@@mel.module "bitcoinjs-lib"] [@@mel.scope "script"]
