ffedd9c628b6848a25e8ba2e7cecbdf5f6524017
0x6A6471 <hello@jakequinter.io>
2025-04-10 17:40:17 -0500

    feat: bip174

--- src/bip174.ml
+++ src/bip174.ml
@@ -1,7 +1,106 @@
-type psbt_input = {
-  redeemScript : Js.Typed_array.Uint8Array.t option;
-  witnessScript : Js.Typed_array.Uint8Array.t option;
+type key_value = {
+  key : Js.Typed_array.Uint8Array.t;
+  value : Js.Typed_array.Uint8Array.t;
 }
 
-type data = { inputs : psbt_input array }
-type t = { data : data; opts : Types.Psbt.psbt_opts }
+type partial_sig = {
+  pubkey : Js.Typed_array.Uint8Array.t;
+  signature : Js.Typed_array.Uint8Array.t;
+}
+
+type witness_utxo = {
+  script : Js.Typed_array.Uint8Array.t;
+  value : Js.Bigint.t;
+}
+
+type bip32_derivation = {
+  masterFingerprint : Js.Typed_array.Uint8Array.t;
+  pubkey : Js.Typed_array.Uint8Array.t;
+  path : string;
+}
+
+type tap_bip32_derivation = {
+  masterFingerprint : Js.Typed_array.Uint8Array.t;
+  pubkey : Js.Typed_array.Uint8Array.t;
+  path : string;
+  leafHashes : Js.Typed_array.Uint8Array.t array;
+}
+
+type tap_script_sig = {
+  pubkey : Js.Typed_array.Uint8Array.t;
+  signature : Js.Typed_array.Uint8Array.t;
+  leafHash : Js.Typed_array.Uint8Array.t;
+}
+
+type tap_script = { leafVersion : int; script : Js.Typed_array.Uint8Array.t }
+
+type tap_leaf_script = {
+  leafVersion : int;
+  script : Js.Typed_array.Uint8Array.t;
+  controlBlock : Js.Typed_array.Uint8Array.t;
+}
+
+type psbt_input = {
+  partialSig : partial_sig option;
+  nonWitnessUtxo : Js.Typed_array.Uint8Array.t option;
+  witnessUtxo : witness_utxo option;
+  sighashType : int option;
+  redeemScript : Js.Typed_array.Uint8Array.t option;
+  witnessScript : Js.Typed_array.Uint8Array.t option;
+  bip32Derivation : bip32_derivation array option;
+  finalScriptSig : Js.Typed_array.Uint8Array.t option;
+  finalScriptWitness : Js.Typed_array.Uint8Array.t option;
+  porCommitment : string option;
+  tapKeySig : Js.Typed_array.Uint8Array.t option;
+  tapScriptSig : tap_script_sig array option;
+  tapLeafScript : Js.Typed_array.Uint8Array.t array option;
+  tapBip32Derivation : tap_bip32_derivation array option;
+  tapInternalKey : Js.Typed_array.Uint8Array.t option;
+  tapMerkleRoot : Js.Typed_array.Uint8Array.t option;
+  uknownKeyVals : key_value array option;
+}
+
+type tap_leaf = {
+  leafVersion : int;
+  script : Js.Typed_array.Uint8Array.t;
+  depth : int;
+}
+
+type tap_tree = { leaves : tap_leaf array }
+
+type psbt_output = {
+  redeemScript : Js.Typed_array.Uint8Array.t option;
+  witnessScript : Js.Typed_array.Uint8Array.t option;
+  bip32Derivation : bip32_derivation array option;
+  tapTree : tap_tree option;
+  tapInternalKey : Js.Typed_array.Uint8Array.t option;
+}
+
+type input_output_counts = { inputCount : int; outputCount : int }
+
+type 'a transaction = {
+  getInputOutputCounts : unit -> input_output_counts;
+  addInput : 'a -> unit;
+  addOutput : 'a -> unit;
+  toBuffer : unit -> Js.Typed_array.Uint8Array.t;
+}
+
+type global_xpub = {
+  extendedPubkey : Js.Typed_array.Uint8Array.t;
+  masterFingerprint : Js.Typed_array.Uint8Array.t;
+  path : string;
+}
+
+type 'a psbt_global = {
+  unsignedTx : 'a transaction;
+  unkownKeyVals : key_value array option;
+  globalXpub : global_xpub array option;
+}
+
+type 'a data = {
+  inputs : psbt_input array;
+  outputs : psbt_output array;
+  globalMap : 'a psbt_global;
+}
+
+type 'a t = { data : 'a data; opts : Types.Psbt.psbt_opts }
--- src/psbt.ml
+++ src/psbt.ml
@@ -1,25 +1,28 @@
-type hash = String of string | Bytes of Js.uint8Array
+type hash = String of string | Bytes of Js.Typed_array.Uint8Array.t
 type transaction_input = { hash : hash; index : int; sequence : int option }
 
 type psbt_tx_input = {
-  hash : Js.uint8Array;
+  hash : Js.Typed_array.Uint8Array.t;
   index : int;
   sequence : int option;
 }
 
-type transaction_output = { script : Js.uint8Array; value : Js.bigint }
+type transaction_output = {
+  script : Js.Typed_array.Uint8Array.t;
+  value : Js.Bigint.t;
+}
 
 type psbt_tx_output = {
   address : string option;
   script : Js.Typed_array.Uint8Array.t;
-  value : Js.bigint;
+  value : Js.Bigint.t;
 }
 
-type t = Bip174.t
+type 'a t = 'a Bip174.t
 
-external fromHex : string -> ?psbt_opts:Types.Psbt.psbt_opts -> unit -> t
+external fromHex : string -> ?psbt_opts:Types.Psbt.psbt_opts -> unit -> 'a t
   = "fromHex"
 [@@mel.module "bitcoinjs-lib"] [@@mel.scope "Psbt"]
 
-external getTxInputs : t -> psbt_tx_input array = "txInputs" [@@mel.get]
-external getTxOutputs : t -> psbt_tx_output array = "txOutputs" [@@mel.get]
+external getTxInputs : 'a t -> psbt_tx_input array = "txInputs" [@@mel.get]
+external getTxOutputs : 'a t -> psbt_tx_output array = "txOutputs" [@@mel.get]
